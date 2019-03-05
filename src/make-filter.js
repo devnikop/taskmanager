08b84c1d93bfc0cbde3d917dd2d2b1d2ae898133
devnikop <petrov.petrov.ak@gmail.com>
2019-03-05 12:55:55 +0300

    rewrite 'createFilter' function by 'literal template'

--- src/make-filter.js
+++ src/make-filter.js
@@ -7,28 +7,21 @@ const FILTER_COUNT_MAX = 200;
 const TASK_COUNT_MIN = 1;
 const TASK_COUNT_MAX = 7;
 
-const filterLabelClickHandler = () => {
-  const cardElements = boardTasksContainerElement.querySelectorAll(`.card`);
-  for (const value of cardElements) {
-    value.remove();
-  }
+const filterInputClickHandler = (evt) => {
+  if (evt.target.matches(`.filter__input`)) {
+    const cardElements = boardTasksContainerElement.querySelectorAll(`.card`);
+    for (const value of cardElements) {
+      value.remove();
+    }
 
-  for (let i = 0; i < getRandomInt(TASK_COUNT_MIN, TASK_COUNT_MAX); i++) {
-    createTask();
+    for (let i = 0; i < getRandomInt(TASK_COUNT_MIN, TASK_COUNT_MAX); i++) {
+      createTask();
+    }
   }
-};
-
-const renderFilterLabel = (filterId) => {
-  const filterLabelElement = document.createElement(`label`);
-  filterLabelElement.addEventListener(`click`, filterLabelClickHandler);
-  filterLabelElement.classList.add(`filter__label`);
-  filterLabelElement.htmlFor = `filter__${filterId.toLowerCase()} `;
-  filterLabelElement.insertAdjacentHTML(`beforeend`, `${filterId}
-    <span class="filter__${filterId.toLowerCase()}-count">${getRandomInt(FILTER_COUNT_MIN, FILTER_COUNT_MAX)}</span>`);
-  return filterLabelElement;
 };
 
 const filterContainerElement = document.querySelector(`.main__filter`);
+filterContainerElement.addEventListener(`click`, filterInputClickHandler);
 
 export const createFilter = (filterId) => {
   filterContainerElement.insertAdjacentHTML(`beforeend`, `<input
@@ -36,6 +29,9 @@ export const createFilter = (filterId) => {
     type='radio'
     id='filter__${filterId.toLowerCase()}'
     name='filter'
-    checked>`);
-  filterContainerElement.appendChild(renderFilterLabel(filterId));
+    checked>
+    <label class='filter__label' for='filter__${filterId.toLowerCase()}'> ${filterId}
+      <span class="filter__${filterId.toLowerCase()}-count">${getRandomInt(FILTER_COUNT_MIN, FILTER_COUNT_MAX)}</span>
+    </label>
+  `);
 };
