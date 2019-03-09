1b0067052ee6c816e87e86e1da39974fbdb29c2a
devnikop <petrov.petrov.ak@gmail.com>
2019-03-09 10:51:22 +0300

    refactor 'filterClickHandler' in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {createTask} from './make-task.js';
 import {createFilter} from './make-filter.js';
 import {getRandomInt} from './util.js';
+import {removeAllElementsByClass} from './util.js';
 import {task} from './data.js';
 
 const TASK_COUNT = 7;
@@ -17,19 +18,18 @@ const FilterName = new Set([
   `Archive`,
 ]);
 
+const addRandomCountOfTask = (taskCount) => {
+  let tempTasksList = [];
+  for (let i = 0; i < taskCount; i++) {
+    tempTasksList[i] = tasks[i];
+  }
+  boardTasksContainerElement.insertAdjacentHTML(`beforeend`, tempTasksList.join(``));
+};
 
-const filterInputClickHandler = (evt) => {
+const filterClickHandler = (evt) => {
   if (evt.target.matches(`.filter__input`)) {
-    const cardElements = document.querySelectorAll(`.board__tasks .card`);
-    for (const value of cardElements) {
-      value.remove();
-    }
-
-    let tempTasksList = [];
-    for (let i = 0; i < getRandomInt(TASK_COUNT_MIN, TASK_COUNT_MAX); i++) {
-      tempTasksList[i] = tasks[i];
-    }
-    boardTasksContainerElement.insertAdjacentHTML(`beforeend`, tempTasksList.join(``));
+    removeAllElementsByClass(document.querySelectorAll(`.board__tasks .card`));
+    addRandomCountOfTask(getRandomInt(TASK_COUNT_MIN, TASK_COUNT_MAX));
   }
 };
 
@@ -56,5 +56,5 @@ const boardTasksContainerElement = document.querySelector(`.board__tasks`);
 boardTasksContainerElement.insertAdjacentHTML(`beforeend`, tasks.join(``));
 
 const filterContainerElement = document.querySelector(`.main__filter`);
-filterContainerElement.addEventListener(`click`, filterInputClickHandler);
+filterContainerElement.addEventListener(`click`, filterClickHandler);
 filterContainerElement.insertAdjacentHTML(`beforeend`, filters.join(``));
--- src/util.js
+++ src/util.js
@@ -1,1 +1,7 @@
 export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const removeAllElementsByClass = (elements) => {
+  for (const value of elements) {
+    value.remove();
+  }
+};
