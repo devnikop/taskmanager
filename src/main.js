d07f97dc8b78422328a2e20daf5c74b508538c5c
devnikop <petrov.petrov.ak@gmail.com>
2020-03-16 10:51:38 +0300

    add filters

---
+++ src/main.js
@@ -0,0 +1,65 @@
+"use strict";
+
+(function() {
+  // util START
+
+  const getRandomNumber = max => Math.ceil(Math.random() * max);
+
+  // util END
+
+  const getElement = {
+    Selector: {
+      MAIN_FILTER: `.main__filter`
+    },
+
+    get mainFilter() {
+      return document.querySelector(this.Selector.MAIN_FILTER);
+    }
+  };
+
+  const filterSet = [
+    `all`,
+    `overdue`,
+    `today`,
+    `favorites`,
+    `repeating`,
+    `tags`,
+    `archive`
+  ];
+
+  const getNode = htmlString => {
+    const element = document.createElement(`div`);
+    element.insertAdjacentHTML(`beforeend`, htmlString);
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(element);
+    return fragment;
+  };
+
+  const getFilterHtml = (text, count) => `
+    <input
+      type="radio"
+      id="filter__${text}"
+      class="filter__input visually-hidden"
+      name="filter"
+      checked
+      />
+      <label for="filter__${text}" class="filter__label">
+      ${text.toUpperCase()} <span class="filter__${text}-count">${count}</span></label
+    >
+  `;
+
+  const addFilters = () => {
+    const mainFilter = getElement.mainFilter;
+
+    const fragment = document.createDocumentFragment();
+    filterSet.forEach(filter => {
+      const filterHmtl = getFilterHtml(filter, getRandomNumber(20));
+      const filterNode = getNode(filterHmtl);
+      fragment.appendChild(filterNode);
+    });
+
+    mainFilter.appendChild(fragment);
+  };
+
+  addFilters();
+})();
