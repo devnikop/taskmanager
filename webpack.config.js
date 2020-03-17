72eda59cde3b2a7adc8fda3126e6add2d7f0f7cc
devnikop <petrov.petrov.ak@gmail.com>
2020-03-18 00:32:04 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: `babel-loader`
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    publicPath: `http://localhost:8080/`,
+    compress: true,
+    watchContentBase: true
+  }
+};
